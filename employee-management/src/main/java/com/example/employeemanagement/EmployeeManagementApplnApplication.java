098d94363ab2120b740240484874d9dce55fadaf
YugendarM <yugendar2411@gmail.com>
2023-06-27 08:38:14 +0530

    Initialized the employee management repository

---
+++ employee-management/src/main/java/com/example/employeemanagement/EmployeeManagementApplnApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementApplnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementApplnApplication.class, args);
+	}
+
+}
